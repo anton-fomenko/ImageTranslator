09f8447b4fe81a10dcd022eb4dfc1cc6cc39344c
Anton Fomenko <vafon2@gmail.com>
2021-06-16 10:33:50 +0300

    Use Google API to detect the text.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,9 +1,14 @@
-﻿using ImageTranslator.Models;
+﻿using Google.Apis.Auth.OAuth2;
+using Google.Cloud.Storage.V1;
+using Google.Cloud.Vision.V1;
+using ImageTranslator.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -28,10 +33,35 @@ namespace ImageTranslator.Controllers
             return View();
         }
 
+        [HttpPost]
+        public IActionResult UploadImage(IFormFile uploadedFile)
+        {
+            ImageAnnotatorClient client = ImageAnnotatorClient.Create();
+            byte[] bytes = GetByteArrayFromFile(uploadedFile);
+            Image image = Image.FromBytes(bytes);
+            IReadOnlyList<EntityAnnotation> textAnnotations = client.DetectText(image);
+
+            foreach (EntityAnnotation text in textAnnotations)
+            {
+                Debug.WriteLine($"Description: {text.Description}");
+            }
+
+            return View("Index");
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        private byte[] GetByteArrayFromFile(IFormFile file)
+        {
+            using (var target = new MemoryStream())
+            {
+                file.CopyTo(target);
+                return target.ToArray();
+            }
         }
     }
 }
